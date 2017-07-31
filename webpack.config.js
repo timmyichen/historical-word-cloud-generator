e65d4525a74862ce97d4760ac0c29f1b88e9bc46
Tim Chen <timmy.i.chen@gmail.com>
2017-07-31 16:33:49 +0000

    trying to fix weird bug 3

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ var config = {
   entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module : {
     loaders : [
@@ -20,7 +21,5 @@ var config = {
     ]
   }
 }
-
-console.log(config.output.path)
 
 module.exports = config;
